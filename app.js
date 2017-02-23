80361cb7cd0e33d38dded64758878d1e8f71aa6d
ksavakia <ksavakia@uncc.edu>
2017-02-23 13:44:04 -0500

    Added angular JS module

---
+++ app.js
@@ -0,0 +1,7 @@
+//immediate invoked function---no local variable blee in the local scope
+(function (){
+    'use strict';
+angular.module('myfirstapp', [])
+.controller('MyFirstController', function(){
+});
+})();
